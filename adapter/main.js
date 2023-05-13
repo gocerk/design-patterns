8b3b1b1eca24effe02e4fad54cf485aaa60c07da
kaanfkennedy <dev.kaanfkennedy@gmail.com>
2023-05-13 12:07:54 +0300

    fix typo

--- adapter/main.js
+++ adapter/main.js
@@ -2,7 +2,7 @@ const CalculatorAdapter = require('./calculatorAdapter');
 
 (() => {
     const calculator = new CalculatorAdapter();
-    let res = calculator.operataion(1, 2, 'add');
+    let res = calculator.operation(1, 2, 'add');
 
     console.log(res);
 })();
